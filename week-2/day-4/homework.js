0938df7e1ff952af0f6446c2325d97910a13a5c2
Meleah Edquid <meleahdy@gmail.com>
2020-11-09 07:55:43 -0800

    updated css and js files

--- week-2/day-4/homework.js
+++ week-2/day-4/homework.js
@@ -1,14 +1,25 @@
 var currentPosition = 0;
-            
-var nextImage = function() {
-      currentPosition = currentPosition - 360;
-      document.getElementById('imageSlider2').style.transform = 'translateX(' + currentPosition + 'px)'}
-           
-var prevImage = function() {
-      currentPosition = currentPosition + 360;
-      document.getElementById('imageSlider2').style.transform = 'translateX(' + currentPosition + 'px)'}
 
-/* setTimeout(nextImage, 360)     
-setTimeout(nextImage, 720)
-setTimeout(nextImage, 1080)
-setTimeout(nextImage, 1440) */
+var nextImage = function () {
+      if (currentPosition > -1440) {
+            currentPosition = currentPosition - 360;
+            document.getElementById('imageSlider2').style.transform = 'translateX(' + currentPosition + 'px)'
+      }
+      console.log(currentPosition)
+}
+
+var prevImage = function () {
+      if (currentPosition < 0) {
+            currentPosition = currentPosition + 360;
+            document.getElementById('imageSlider2').style.transform = 'translateX(' + currentPosition + 'px)'
+      }
+      console.log(currentPosition)
+}
+
+if (currentPosition > -1440) {
+      setInterval(nextImage, 5000)
+} 
+
+// else if (currentPosition < 0) {
+//       setInterval(prevImage, 5000)
+// }
